867ce12a0df67f75de8bcdab5e58a916ea00624e
Yanrong <suga_e@outlook.com>
2022-02-23 17:18:38 +0000

    complete FK.m

--- SA-1/FK.m
+++ SA-1/FK.m
@@ -18,23 +18,52 @@ end
 % initialisation
 T = eye(4,4);
 J = zeros(6,n);
+Tn = [T];
 %% T homogeneous matrix calculations
 for i = 1:n
-    if jtype(i)==1  % prismatic
+    a = DH_params(i, 1);
+    alph = DH_params(i, 2);
+    c_alpha = cos(alph);
+    s_alpha = sin(alph);
+
+    if jtype(i) == 1  % prismatic
         %%%% complete here %%%%
+        d = DH_params(i, 3);
+        A_i = [
+            1, 0, 0, a;
+            0, c_alpha, -s_alpha, 0;
+            0, s_alpha, c_alpha, d;
+            0, 0, 0, 1];
     else            % revolute
         %%%% complete here %%%%
+        theta = DH_params(i, 4);
+        c_theta = cos(theta);
+        s_theta = sin(theta);
+        A_i = [
+            c_theta, -s_theta*c_alpha, s_theta*s_alpha, a*c_theta;
+            s_theta, c_theta*c_alpha, -c_theta*s_alpha, a*s_theta;
+            0, s_alpha, c_alpha, 0;
+            0, 0, 0, 1];
     end
     %%%% complete here %%%%
     % computing Homogeneous matrix
+    T = T * A_i;
     %%%% complete here %%%%
+    Tn = cat(3, Tn , T); % save intermediate for J
 end
 
-%% J Jacobian calculations
-for i = 1:n  
-    if jtype(i)==1  %prismatic
+%% J Jacobian calculations89
+for i = 1:n
+    Z_i = Tn(1:3,3,i);
+    if jtype(i) == 1  % prismatic
         %%%% complete here %%%%
+        J(:,i) = [
+            Z_i; % linear velocity
+            0; 0; 0]; % angular velocity
     else             % revolute
         %%%% complete here %%%%
-    end  
+        J(:,i) = [
+            cross(Z_i, T(1:3,4)-Tn(1:3,4,i)); % linear velocity
+            Z_i]; % angular velocity
+    end
 end
